3fcd7344054df9073212775350c00b331dd6530a
Rajesh Kumar <thoughtlesspaper796@age.sh>
2026-04-04 18:34:28 +0000

    chore: add .gitignore, remove target artifacts

--- target/debug/add_one_plus_one.d
+++
@@ -1,1 +0,0 @@
-/tmp/tmp.Oh6G9ehXRD/add_one_plus_one/target/debug/add_one_plus_one: /tmp/tmp.Oh6G9ehXRD/add_one_plus_one/src/main.rs
--- target/debug/deps/add_one_plus_one-07a935b37aadfbbe.d
+++
@@ -1,5 +0,0 @@
-/tmp/tmp.Oh6G9ehXRD/add_one_plus_one/target/debug/deps/add_one_plus_one-07a935b37aadfbbe.d: src/main.rs
-
-/tmp/tmp.Oh6G9ehXRD/add_one_plus_one/target/debug/deps/add_one_plus_one-07a935b37aadfbbe: src/main.rs
-
-src/main.rs:
